b016c2b4185f697d0419a4286a38ea767fa58134
danielsarj <dnsarj@gmail.com>
2023-04-28 13:07:15 -0500

    update example command scritps

--- DIY_scripts/01_run_MatrixeQTL_example_command.sh
+++ DIY_scripts/01_run_MatrixeQTL_example_command.sh
@@ -4,12 +4,12 @@ for pop in GBR YRI
 do
   for chr in 22
   do
-    Rscript ~/TOPMed_MESA_crosspop_portability/DIY_scripts/01_run_MatrixeQTL.R \
-    -d ~/TOPMed_MESA_crosspop_portability/sample_data/dosages/GEUVADIS_${pop}_chr${chr}_dosage_filtered.txt.gz \
-    -e ~/TOPMed_MESA_crosspop_portability/sample_data/exp_tbls/GEUVADIS_${pop}_expression.txt.gz \
-    -g ~/TOPMed_MESA_crosspop_portability/sample_data/gene_annotation.txt \
+    Rscript /DIY_scripts/01_run_MatrixeQTL.R \
+    -d sample_data/dosages/GEUVADIS_${pop}_chr${chr}_dosage_filtered.txt.gz \
+    -e sample_data/exp_tbls/GEUVADIS_${pop}_expression.txt.gz \
+    -g sample_data/gene_annotation.txt \
     -t ${pop}_chr${chr}_cis \
-    -o ~/TOPMed_MESA_crosspop_portability/sample_data/cis_ES/ \
+    -o sample_data/cis_ES/ \
     -w 1000000
   done
 done
--- DIY_scripts/02_prepare_MASHR_inputs_example_command.sh
+++ DIY_scripts/02_prepare_MASHR_inputs_example_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Rscript ~/TOPMed_MESA_crosspop_portability/DIY_scripts/02_prepare_MASHR_inputs.R \
--l ~/TOPMed_MESA_crosspop_portability/sample_data/chr22_files.txt \
--g ~/TOPMed_MESA_crosspop_portability/sample_data/gene_annotation.txt \
+Rscript DIY_scripts/02_prepare_MASHR_inputs.R \
+-l sample_data/chr22_files.txt \
+-g sample_data/gene_annotation.txt \
 -c 22 \
--o ~/TOPMed_MESA_crosspop_portability/sample_data/MASHR_inputs
+-o sample_data/MASHR_inputs
